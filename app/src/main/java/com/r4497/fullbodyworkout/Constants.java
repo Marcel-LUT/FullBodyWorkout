72a12d340f104e0479023441ba2d9999c87551e1
Marcel <marcel.pflugfelder@student.lut.fi>
2020-04-19 22:18:52 +0200

    Get Location Permission / Get Location / Edit SearchBar

---
+++ app/src/main/java/com/r4497/fullbodyworkout/Constants.java
@@ -0,0 +1,7 @@
+package com.r4497.fullbodyworkout;
+
+public class Constants {
+
+    private static final int PERMISSIONS_REQUEST_ENABLE_GPS = 9002;
+    private static final int PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION = 9003;
+}
